82fcaeee3c5cfcb74506d4cb9b27676e13cfbf26
Artem Pavlov <timoha26.88@gmail.com>
2026-07-14 18:07:16 +0700

    fixed bugs

--- src/app/conf/conf.production.ts
+++ src/app/conf/conf.production.ts
@@ -1,6 +1,6 @@
 import {ConfModel} from "../models/conf.model";
 
 export const conf: ConfModel = {
-  trainerProUrl: "http://trainerpro.production",
-  filesUrl: "http://minio-api.production"
+  trainerProUrl: "http://trainerpro.local",
+  filesUrl: "http://minio-api.local"
 }
--- src/app/conf/conf.ts
+++ src/app/conf/conf.ts
@@ -1,7 +1,6 @@
 import {ConfModel} from "../models/conf.model";
 
 export const conf: ConfModel = {
-  //trainerProUrl: "http://trainerpro.local",
   trainerProUrl: "http://localhost:8000",
   filesUrl: "http://minio-api.local"
 }
